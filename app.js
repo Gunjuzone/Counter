82e07b7d4b4301794047c95fed1a0714608d590f
gunjuzone <Gunjuzone@gmail.com>
2021-07-26 12:08:36 +0100

    Set up EventListener on the buttons

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let counter = 0;
 const value = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
 
-// Loop through the buttons and add event listener to the forEach function
+// Loop through the buttons and add event listener to the forEach function. Use the event object (e) to check which of the button you are clicking
 
 btns.forEach(function (btn) {
-  console.log(btn);
+  btn.addEventListener('click', function (e) {
+    console.log(e.currentTarget);
+  });
 });
