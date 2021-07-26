d09cc1ec8eba750f0007b2a377a66d85b9ade504
gunjuzone <Gunjuzone@gmail.com>
2021-07-26 11:14:21 +0100

    Select buttons and loop through the buttons

---
+++ app.js
@@ -0,0 +1,11 @@
+//Declare an empty variable that take our counter
+let counter = 0;
+// Select the buttons and the initial counter value
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+// Loop through the buttons and add event listener to the forEach function
+
+btns.forEach(function (btn) {
+  console.log(btn);
+});
